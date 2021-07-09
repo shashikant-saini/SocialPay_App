4c7ad515693f27ee4c6efc198ad094a57955ebf2
shashikant-saini <44373858+shashikant-saini@users.noreply.github.com>
2021-07-10 00:24:30 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -65,22 +65,7 @@ final kBoxDecor = BoxDecoration(
 );
 
 
-const credentials = r'''
-  {
-  "type": "service_account",
-  "project_id": "socialpaydata",
-  "private_key_id": "378954070ab969076e99f73c5846a27f8256d4b0",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCSfaZNmufOXFHV\nByspoAfoEKTFk416G09T6rGs3rXvYad9ArUeNWy3m2MSWF910HNAmKxW3h6sP9Vp\npPFICBDt8utmZrOoGp2HINu1i6rK6bPB1R66kgWJ3siOjTdYgFe0apd/1ydGLYlF\nyKyIadTurfGBFOaIdvnvvPCgpcCwwyWfKoDOYi31QME6NcrGKiH19ctMPpZp/oL+\nZZPrHBhMjGldR+zqzIKhC//4UhHlTmjvEcXH34znoQ6ObBDtbFirLIZKRHKa4aV9\nS2oyWyfMM4kmlNNtdDurTsEmnAEDm6L32YpmgSHdLENkBK+aRfb2/QMpWUsMM3zO\naxikv97FAgMBAAECggEACJ5jkEb6uB2euOxfjJGRoC8z1Ttdrwn39wV6Ek7WIHE7\n6Uh6qdOAg9BHb2xgosNdYeG5qLC6gu2Mhlcsf6Lt2roYlyFCv7Nf156bc/PK3CkM\n05d7joQ0gYyTJdlGy3+y6Uv+LDOB6DNEqM+U4GptmzoDrqcGpeUYxmifiwFwNAC9\nxyiDiw2EQkPT9IqMvstIXKql64KgweRjrxVxaCuFVWvI27o9bjas8ekyOzlFMJqa\nPaBHNYQ5jMCz2xs6T/oFs3SCTEYJX33axoL40tJmWhOyinxF1AfWrvWvuESuHccB\njT0UU1iZnhj8g12Z/H0rmAJ8Vhj/L5f7g85d/WrYAQKBgQDNWM22ZxeppuzJoel4\n+chGmzVT1tv9sL1mOQR1oKtKyV/VhqwTUkfX7zm3m7rDMJ2xZNKnzNxe0rK2FKg1\nftql5tcrPO0q9G4rDXtyhG7jaLjqgMAwbr+QiNknlUCNKYQnPqHzYyFVlLsRSfXA\nBeBoQ+y6GaRZncBFnLgYnwd8AQKBgQC2oDeKum7j+HQ9273ulaOye2Id1vft/jGS\nRvV2fHvKYn1qGTdrkyIj5nNXqNUGgDWc2JEZ5mEy87qYn7bQ+8w+vFoyVaG1Kdav\nS15Nfq47ytD3iSZx9Gdco2zYogz2LxxdC3ghb7AobyzvutI34vkwCIoJwAvgeR34\nkQOagsVyxQKBgEzlunNZ+9R+PCHtzh6Ne2Blcvzocw13iBWluHPMLRkefBLcEchS\ngbDtVvHZEYFSzVrG5NQNtWCVCnOBHNSboomJ/n2knTP+wNrCe74Qte3XI5g9irsY\nXghkFt3p2HiEWYbQv5kDQFJqFfuNdMfp63VgKmAuhgtBNeVboN0ngIgBAoGAQ9Qr\n7N2jlA5SdisoVvXDD+ZdocGW4hD4LmEmh/RiMd/0vrRQ7iZ+lqDgFB3DyR9TSkbx\nVZJIZZk6o7cmOmZbDK3+PhWp8tRwRTkT9GppohGJHXcWDVr+DN0x7x/4+nKy8Sfy\nXVDTN6FbKkrUCyfE738sbcSjZKL3a7vIf9m0JhUCgYEAvrX58Ef7EHRQcjtzRJyS\nWeqdAlDWXEI6NZcHFNdG/UWVLtsN7BATgBCYjdtLYWR3e7j6Ltka2Vf7XRyPynZm\nJ7+lOc1ElAaDkaTqWwOpaJkQeeleVBEEG5K1bYwbkhYvN110hesjIt4g1nSAvfLX\nXANBU2QERN8OsBPR7AtclRw=\n-----END PRIVATE KEY-----\n",
-  "client_email": "socialpaydata@socialpaydata.iam.gserviceaccount.com",
-  "client_id": "103238198173531707492",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/socialpaydata%40socialpaydata.iam.gserviceaccount.com"
-  }
-  ''';
-
-const spreadsheetId = "1jk33XBdt2YHI26TAan1W0PhCr8Xmzed8_PHVzu3JyI0";
+const credentials = r''''''
 
 class DialogHelper{
   static exit(context) => showDialog(context: context, builder: (context) => dialogBox());
